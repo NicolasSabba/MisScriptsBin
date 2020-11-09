2e8e14db02bf09239d035e3b5201fe626355ade3
nicolas <nicolas.sabbatini.95@gmail.com>
2020-11-09 18:21:10 -0300

    update

--- bin/update.sh
+++ bin/update.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
+set -e
+
+function npm_update {
+	for package in $(npm -g outdated --parseable --depth=0 | cut -d: -f3)
+	do
+    		echo "Actualizando: $package"
+		npm -g install "$package"
+	done
+}
+
 echo 'Actualizando el sistema'
 sudo apt update
 sudo apt upgrade -y
 echo 'Actualizando npm'
-sudo npm update -g 
+npm_update
+echo 'Actualizando snap'
+sudo snap refresh
+echo 'Actualizando flatpak'
+flatpak update
+echo 'Actualizando rust ⚙'
+rustup update
 
